19123f264cf36b63512ea50d5c5b516a4a9cfee9
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-07-24 18:07:13 -0700

    Update data_extraction.py

--- data_extraction.py
+++ data_extraction.py
@@ -1,11 +1,24 @@
 import os
+import boto3
 from PIL import Image
 import pandas as pd
 import numpy as np
 
 def load_data():
-    normal_dir = 'Normal'
-    tb_dir = 'Tuberculosis'
+    s3 = boto3.client('s3')
+    bucket_name = 'usecases-data'
+    url = s3.generate_presigned_url(
+                    ClientMethod='get_object',
+                    Params={'Bucket': bucket_name, 'Key': 'tuberculosis_xray_data.zip'},
+                    ExpiresIn=7200  # URL expiration time in seconds (adjust as needed)
+                )
+    print(url)
+    url_response = requests.get(url)
+    with zipfile.ZipFile(BytesIO(url_response.content)) as z:
+        z.extractall('.')
+    
+    normal_dir = 'tuberculosis_xray_data/Normal'
+    tb_dir = 'tuberculosis_xray_data/Tuberculosis'
     
     normal_files = [os.path.join(normal_dir, f) for f in os.listdir(normal_dir) if os.path.isfile(os.path.join(normal_dir, f))]
     tb_files = [os.path.join(tb_dir, f) for f in os.listdir(tb_dir) if os.path.isfile(os.path.join(tb_dir, f))]

c68106b112e52cd464eaefa1ad781efdefa65963
Nix-Z <49776148+Nix-Z@users.noreply.github.com>
2024-07-28 18:21:40 -0700

    Update data_transformation.py

--- data_transformation.py
+++ data_transformation.py
@@ -13,13 +13,12 @@ from datavisualization import visualize_data
 def transform_data():
     path, normal_images, tuberculosis_images = visualize_data()
     
-    data_transform = torchvision.transforms.Compose(
-    [
+    data_transform = torchvision.transforms.Compose([
         transforms.Resize(256),
         transforms.CenterCrop(224),
         transforms.ToTensor(),
         transforms.Normalize(mean=[0.485, 0.456, 0.406], std=[0.229, 0.224, 0.225]),
-    ])
+        ])
     total_list = normal_images + tuberculosis_images
     model_dataset = datasets.ImageFolder(path, transform=data_transform)
     img, ann = model_dataset[0]
